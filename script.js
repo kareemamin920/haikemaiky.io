9215ad5660dcd4a28c9e16b76fb4ad990d7fc5ce
kareemamin920 <kareempsnet2@gmail.com>
2026-02-10 12:52:25 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    const navLinks = document.getElementById('navLinks');
+    // إضافة أو إزالة كلاس 'active' لفتح وإغلاق القائمة
+    navLinks.classList.toggle('active');
+}
